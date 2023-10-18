4f67f626d8613de1bb05bc6ed02ad3bedbf1450b
firapinch <firapinch@gmail.com>
2023-10-18 08:03:06 -0400

    optimized

--- src/main/java/com/firapinch/controller/CharacterFrequencyController.java
+++ src/main/java/com/firapinch/controller/CharacterFrequencyController.java
@@ -4,37 +4,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RestController
 public class CharacterFrequencyController {
 
     @GetMapping("/calculate-frequency")
-    public Map<Character, Integer> calculateFrequency(@RequestParam String input) {
+    public Map<Character, Long> calculateFrequency(@RequestParam String input) {
         if (input == null || input.isEmpty()) {
             return Map.of();
         }
 
-        Map<Character, Integer> frequencyMap = input.chars()
+        return input.chars()
                 .mapToObj(c -> (char) c)
-                .collect(Collectors.toMap(
-                        character -> character,
-                        character -> 1,
-                        Integer::sum
-                ));
-
-        return frequencyMap.entrySet().stream()
-                .sorted((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()))
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (e1, e2) -> e1,
-                        LinkedHashMap::new
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
                 ));
     }
 }
-
-
-
